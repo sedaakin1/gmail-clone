9d568c4096f3b686007833854b546ca5538fe4f8
Seda Akin <dalyanogluseda@gmail.com>
2025-01-29 00:51:06 +0300

    .

--- src/components/Leftpanel.js
+++ src/components/Leftpanel.js
@@ -4,86 +4,92 @@ import { RiInboxFill } from "react-icons/ri";
 import { FaRegStar, FaRegClock, FaRegFile } from "react-icons/fa6";
 import { MdLabelImportantOutline , MdExpandMore } from "react-icons/md";
 import { BiSend } from "react-icons/bi";
+import { fontWeight } from '@mui/system';
 
 const Leftpanel = () => {
   return (
-    <div style={{backgroundColor:"#F9F9F9", minHeight:"100vh", color:"#3C3C3C"}}>
+    <div style={{backgroundColor:"#F9F9F9", width:"170px",minHeight:"100vh", color:"#3C3C3C"}}>
       <div style={{
         display:"flex", 
         alignItems:"center", 
         borderRadius:"20px",
         backgroundColor:"#c2e7ff",
-        width:"10vw",
-        marginLeft:"1vw"
+        margin:"0 10px"
         }}>
-        <CreateOutlinedIcon style={{width:"1.2vw", marginLeft:"2vw"}}/> 
-        <h4 style={{marginLeft:"1.6vw"}}>Compose</h4>
+        <CreateOutlinedIcon style={{width:"1.2vw",marginLeft:"20px"}}/> 
+        <h4 style={{fontWeight:"500"}}>Compose</h4>
       </div>
 
       <div style={{
         display:"flex", 
         alignItems:"center", 
         borderRadius:"40px",        
-        width:"13vw",
-        backgroundColor:"#d3e3fd"        
+        width:"250px"     
         }}>
       <RiInboxFill style={{width:"1.2vw", marginLeft:"2vw"}}/>
-      <h4 style={{marginLeft:"1.6vw"}}>Inbox</h4>
+      <span style={{marginLeft:"1.6vw", fontWeight:"500"}}>Inbox</span>
       </div>
 
       <div style={{
         display:"flex", 
         alignItems:"center", 
-             
+        borderRadius:"40px",        
+        width:"250px"
         }}>
       <FaRegStar style={{width:"1.2vw", marginLeft:"2vw"}}/>
-      <h4 style={{marginLeft:"1.6vw"}}>Starred</h4>
+      <span style={{marginLeft:"1.6vw", fontWeight:"500"}}>Starred</span>
       </div>
 
       <div style={{
         display:"flex", 
         alignItems:"center", 
-              
+        borderRadius:"40px",        
+        width:"250px",
         }}>
       <FaRegClock style={{width:"1.2vw", marginLeft:"2vw"}}/>
-      <h4 style={{marginLeft:"1.6vw"}}>Snoozed</h4>
+      <span style={{marginLeft:"1.6vw", fontWeight:"500"}}>Snoozed</span>
       </div>
 
       <div style={{
         display:"flex", 
         alignItems:"center", 
-              
+        borderRadius:"40px",        
+        width:"250px",    
         }}>
       <MdLabelImportantOutline style={{width:"1.2vw", marginLeft:"2vw"}}/>
-      <h4 style={{marginLeft:"1.6vw"}}>Important</h4>
+      <span style={{marginLeft:"1.6vw", fontWeight:"500"}}>Important</span>
       </div>
 
       <div style={{
         display:"flex", 
         alignItems:"center", 
-              
+        borderRadius:"40px",        
+        width:"250px",    
         }}>
       <BiSend style={{width:"1.2vw", marginLeft:"2vw"}}/>
-      <h4 style={{marginLeft:"1.6vw"}}>Sent</h4>
+      <span style={{marginLeft:"1.6vw", fontWeight:"500"}}>Sent</span>
       </div>
 
       <div style={{
         display:"flex", 
         alignItems:"center", 
-              
+        borderRadius:"40px",        
+        width:"250px",   
         }}>
       <FaRegFile style={{width:"1.2vw", marginLeft:"2vw"}}/>
-      <h4 style={{marginLeft:"1.6vw"}}>Drafts</h4>
+      <span style={{marginLeft:"1.6vw", fontWeight:"500"}}>Drafts</span>
       </div>
 
       <div style={{
         display:"flex", 
         alignItems:"center", 
-              
+        borderRadius:"40px",        
+        width:"250px",   
         }}>
       <MdExpandMore style={{width:"1.2vw", marginLeft:"2vw"}}/>
-      <h4 st le={{marginLeft:"1.6vw"}}>More</h4>
+      <span style={{marginLeft:"1.6vw", fontWeight:"500"}}>More</span>
       </div>
+      
     </div>
   )
 }
